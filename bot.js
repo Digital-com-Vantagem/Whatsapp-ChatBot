f12adfb46c4d70a60224c138069a3e9b59b3bc47
Digital-com-Vantagem <144627846+Digital-com-Vantagem@users.noreply.github.com>
2023-10-28 16:06:34 -0300

    bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,8 @@
 const http = require('http');
-const qrcode = require('qrcode');
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
 
-// Create an HTTP server
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  // Generate QR code and serve it within the HTML response
-  client.on('qr', (qr) => {
-    qrcode.toDataURL(qr, (err, url) => {
-      if (err) {
-        console.error(err);
-        res.end('Error generating QR code');
-      } else {
-        const htmlWithQR = `
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <title>WhatsApp QR Code</title>
-            </head>
-            <body>
-              <h1>WhatsApp QR Code</h1>
-              <img src="${url}" alt="QR Code" />
-            </body>
-          </html>
-        `;
-        res.end(htmlWithQR);
-      }
-    });
-  });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
 });
 
 const port = process.env.PORT || 3000;
@@ -38,14 +11,23 @@ server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-client.on('ready', () => {
-  console.log('Client is ready!');
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
 });
 
-client.on('message', (message) => {
-  if (message.body === '!ping') {
-    client.sendMessage(message.from, 'pong');
-  }
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	if(message.body === '!ping') {
+		client.sendMessage(message.from, 'pong');
+	}
 });
 
 client.initialize();
